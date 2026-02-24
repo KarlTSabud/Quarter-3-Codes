9b432c046ec452e6f3007db4e0f38664e198377b
KarlTSabud <karlgian.sabud@smc.pshs.edu.ph>
2026-02-24 21:05:44 +0800

    Add student record input and output functionality

---
+++ Sabud_Q3_FA06.py
@@ -0,0 +1,13 @@
+# create empty dictionary
+student = {}
+
+# get input
+student["name"] = input("Enter your name: ")
+student["age"] = input("Enter your age: ")
+student["subject"] = input("Enter your favorite subject: ")
+
+# print output
+print("\nStudent Record:")
+print("Name:", student["name"])
+print("Age:", student["age"])
+print("Favorite Subject:", student["subject"])
